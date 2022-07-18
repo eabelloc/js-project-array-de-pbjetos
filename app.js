d981f38718be87517bea23f37807ae00b11d06e4
Elias Bello <eabelloc.dev@gmail.com>
2022-07-18 21:31:02 +0200

    Music Classical and Jazz Genres List ready

---
+++ app.js
@@ -0,0 +1,104 @@
+const albumsClassical = [
+    {
+        name: "Xenakis: Selected Works",
+        year: 2022,
+        studio: "UMG Recording, Inc.",
+        artist: "Iannis Xenakis",
+        img: "",
+    },
+    {
+        name: "D'Haene: Music With Silent Aitake's",
+        year: 2019,
+        studio: "Ravello Records",
+        artist: "Frédéric D'Haene",
+        img: "", 
+    },
+    {
+        name: "Ives: Orchestral Works, Vol. 3",
+        year: 2017,
+        studio: "Chandos",
+        artist: "Charles Ives",
+        img: "", 
+    },
+    {
+        name: "Steve Reich - The ECM Recordings",
+        year: 2016,
+        studio: "ECM Records GmbH",
+        artist: "Steve Reich",
+        img: "", 
+    },
+    {
+        name: "Vivier Lonely Child; Prologue pour un Marco Polo; Bouchara; Zipangu",
+        year: 2016,
+        studio: "Decca Music Group Limited",
+        artist: "Claude Vivier",
+        img: "", 
+    },
+    {
+        name: "Scelsi: The Orchestral Works, Vol. 2",
+        year: 2007,
+        studio: "Mode Records",
+        artist: "Giacinto Scelsi",
+        img: "", 
+    },
+    {
+        name: "Grisey: Les espaces acoustiques",
+        year: 2005,
+        studio: "Kairos",
+        artist: "Gérard Grisey", 
+    },
+]
+
+const albumsJazz = [
+    {
+        name: "Harmony of Difference",
+        year: 2017,
+        studio: "Dom Chi No",
+        artist: "Kamasi Washington", 
+    },
+    {
+        name: "Mambo Nights",
+        year: 2016,
+        studio: "Termidor",
+        artist: "Arturo Sandoval", 
+    },
+    {
+        name: "The Duke (Remastered 2014)",
+        year: 2014,
+        studio: "Bethlehem Records",
+        artist: "Duke Ellington", 
+    },
+    {
+        name: "Jazz From Hell",
+        year: 1986,
+        studio: "Zappa Family Trust",
+        artist: "Frank Zappa", 
+    },
+    {
+        name: "Pastel Blues",
+        year: 1965,
+        studio: "UMG Recordings, Inc.",
+        artist: "Nina Simone", 
+    },
+    {
+        name: "Out To Lunch (The Rudy Van Gelder Edition)",
+        year: 1964,
+        studio: "Blue Note Records",
+        artist: "Eric Dolphy", 
+    },
+    {
+        name: "The Black Saint And The Sinner Lady",
+        year: 1963,
+        studio: "The Verve Music Group (Division of UMG Recordings, Inc.)",
+        artist: "Charles Mingus", 
+    },
+]
+
+const albumsElectronic = [
+    {
+        name: "Vulnicura",
+        year: 2015,
+        studio: "One Little Independent Records",
+        artist: "Björk", 
+    },  
+]
