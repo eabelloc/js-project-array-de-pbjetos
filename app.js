5097a75f685e708b7a6b8195158f672eeb8e644a
Elias Bello <eabelloc.dev@gmail.com>
2022-07-19 02:26:49 +0200

    HTML-JS-Finalizado--CSS-Aun-no

--- app.js
+++ app.js
@@ -1,104 +1,250 @@
-const albumsClassical = [
+const classical = [
     {
         name: "Xenakis: Selected Works",
         year: 2022,
         studio: "UMG Recording, Inc.",
         artist: "Iannis Xenakis",
-        img: "",
+        image: "https://www.dibpic.com/uploads/posts/2022-02/1645413700_folder.jpg",
     },
     {
         name: "D'Haene: Music With Silent Aitake's",
         year: 2019,
         studio: "Ravello Records",
         artist: "Frédéric D'Haene",
-        img: "", 
+        image: "https://m.media-amazon.com/images/I/91nbrd1rXjL._SS500_.jpg", 
     },
     {
         name: "Ives: Orchestral Works, Vol. 3",
         year: 2017,
         studio: "Chandos",
         artist: "Charles Ives",
-        img: "", 
+        image: "https://m.media-amazon.com/images/I/81giiTaH93L._SY355_.jpg", 
     },
     {
         name: "Steve Reich - The ECM Recordings",
         year: 2016,
         studio: "ECM Records GmbH",
         artist: "Steve Reich",
-        img: "", 
+        image: "https://m.media-amazon.com/images/I/61rAQgMe+zL._SL1400_.jpg", 
     },
     {
-        name: "Vivier Lonely Child; Prologue pour un Marco Polo; Bouchara; Zipangu",
+        name: "Vivier: Lonely Child; Prologue pour un Marco Polo; Bouchara; Zipangu",
         year: 2016,
         studio: "Decca Music Group Limited",
         artist: "Claude Vivier",
-        img: "", 
+        image: "https://m.media-amazon.com/images/I/81ylclsrXgL._SS500_.jpg", 
     },
     {
         name: "Scelsi: The Orchestral Works, Vol. 2",
         year: 2007,
         studio: "Mode Records",
         artist: "Giacinto Scelsi",
-        img: "", 
+        image: "https://m.media-amazon.com/images/I/41LX3rMpkRL._SX355_.jpg", 
     },
     {
         name: "Grisey: Les espaces acoustiques",
         year: 2005,
         studio: "Kairos",
-        artist: "Gérard Grisey", 
+        artist: "Gérard Grisey",
+        image: "https://www.kairos-music.com/sites/default/files/cds/0012422KAI_Grisey_webcover_0.jpg", 
+    },
+    {
+        year: 2022,
+        studio: "Self Produced",
+        artist: "Ludovico Einaudi",
+        image: "https://images-eu.ssl-images-amazon.com/images/I/31AriBG8mVL._SY264_BO1,204,203,200_QL40_ML2_.jpg", 
     },
 ]
 
-const albumsJazz = [
+const jazz = [
     {
         name: "Harmony of Difference",
         year: 2017,
         studio: "Dom Chi No",
-        artist: "Kamasi Washington", 
+        artist: "Kamasi Washington",
+        image: "https://media1.jpc.de/image/w600/front/0/0889030017116.jpg", 
     },
     {
         name: "Mambo Nights",
         year: 2016,
         studio: "Termidor",
-        artist: "Arturo Sandoval", 
+        artist: "Arturo Sandoval",
+        image: "https://m.media-amazon.com/images/I/91fMFKR47KL._SS500_.jpg", 
     },
     {
         name: "The Duke (Remastered 2014)",
         year: 2014,
         studio: "Bethlehem Records",
-        artist: "Duke Ellington", 
+        artist: "Duke Ellington",
+        image: "https://m.media-amazon.com/images/I/818S7QQyYGL._SS500_.jpg", 
     },
     {
         name: "Jazz From Hell",
         year: 1986,
         studio: "Zappa Family Trust",
-        artist: "Frank Zappa", 
+        artist: "Frank Zappa",
+        image: "https://m.media-amazon.com/images/I/71vHX4XkjAL._SX355_.jpg", 
     },
     {
         name: "Pastel Blues",
         year: 1965,
         studio: "UMG Recordings, Inc.",
-        artist: "Nina Simone", 
+        artist: "Nina Simone",
+        image: "https://m.media-amazon.com/images/I/41sjfGddWwL.jpg", 
     },
     {
         name: "Out To Lunch (The Rudy Van Gelder Edition)",
         year: 1964,
         studio: "Blue Note Records",
-        artist: "Eric Dolphy", 
+        artist: "Eric Dolphy",
+        image: "https://m.media-amazon.com/images/I/711dScN4DZL._SS500_.jpg", 
     },
     {
         name: "The Black Saint And The Sinner Lady",
         year: 1963,
         studio: "The Verve Music Group (Division of UMG Recordings, Inc.)",
-        artist: "Charles Mingus", 
+        artist: "Charles Mingus",
+        image: "https://m.media-amazon.com/images/I/81hhw07NBDL._SY355_.jpg", 
     },
 ]
 
-const albumsElectronic = [
+const electronic = [
+    {
+        name: "Utopia",
+        year: 2017,
+        studio: "One Little Independent Records",
+        artist: "Björk",
+        image: "https://m.media-amazon.com/images/I/712oyFRZCTL._SY355_.jpg", 
+    },   
     {
         name: "Vulnicura",
         year: 2015,
-        studio: "One Little Independent Records",
-        artist: "Björk", 
+        studio: "",
+        artist: "Björk",
+        image: "https://m.media-amazon.com/images/I/71EG3iJfg3S._SY355_.jpg", 
     },  
+    {
+        name: "Mutant",
+        year: 2015,
+        studio: "Self Produced",
+        artist: "Arca",
+        image: "https://m.media-amazon.com/images/I/61UgILvtb8L._SX355_.jpg", 
+    },
+    {
+        name: "Hold",
+        year: 2019,
+        studio: "Self Produced",
+        artist: "HOME",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/b5/f6/55/b5f655dc-7805-938b-1716-92e74c081200/artwork.jpg/600x600bb.webp", 
+    },
+    {
+        name: "Odyssey",
+        year: 2014,
+        studio: "Self Produced",
+        artist: "HOME",
+        image: "https://m.media-amazon.com/images/I/A1gQOjL9u8L._SS500_.jpg", 
+    },
+    {
+        name: "Selected Ambient Works 85-92",
+        year: 1992,
+        studio: "R&S Records",
+        artist: "Aphex Twin",
+        image: "https://m.media-amazon.com/images/I/51TOvM8vU7L._SY355_.jpg", 
+    },      
 ]
+
+const postRock = [
+    {
+        name: "0",
+        year: 2014,
+        studio: "Tonequake Records",
+        artist: "Low Roar",
+        image: "https://m.media-amazon.com/images/I/61Bz5jeuTZL._SX355_.jpg", 
+    },
+    {
+        name: "Kveikur",
+        year: 2013,
+        studio: "KRUNK",
+        artist: "Sigur Rós",
+        image: "https://media.pitchfork.com/photos/5929a1a4b1335d7bf16982ee/1:1/w_450%2Cc_limit/7b4579cb.jpg", 
+    },
+    {
+        name: "Valtari",
+        year: 2012,
+        studio: "KRUNK",
+        artist: "Sigur Rós",
+        image: "https://media.pitchfork.com/photos/5929b558ea9e61561daa6dce/1:1/w_450%2Cc_limit/053aea55.jpeg", 
+    },
+    {
+        name: "Yanqui U.X.O.",
+        year: 2002,
+        studio: "Constellation",
+        artist: "Godspeed You! Black Emperor",
+        image: "https://media.pitchfork.com/photos/5929bf4cea9e61561daa7a31/1:1/w_450%2Cc_limit/a2766e8a.jpg", 
+    },
+    {
+        name: "F# A# ∞",
+        year: 2002,
+        studio: "kranky",
+        artist: "Godspeed You! Black Emperor",
+        image: "https://media.pitchfork.com/photos/5e4c3426159e01000820c835/1:1/w_450%2Cc_limit/F%25E2%2599%25AF%2520A%25E2%2599%25AF%2520%25E2%2588%259E_Godspeed%2520You!%2520Black%2520Emperor.jpg", 
+    },
+]
+
+const albumsClassical = document.querySelector("#albumsclassical");
+const albumsJazz = document.querySelector("#albumsjazz");
+const albumsElectronic = document.querySelector("#albumselectronic");
+const albumsPostRock = document.querySelector("#albumspostrock");
+
+const iterateList = (list, container) => {
+    for (const element of list) {
+        generateHTML(element, container);
+    }
+};
+
+const generateHTML = (element, container) => {
+    let myFigure;
+    if ("name" in element) {
+        myFigure = `<figure>
+        <h2 class="album-title">${element.name} - ${element.year}</h2>
+        <h3 class="artist">${element.artist}</h3>
+        <img src="${element.image}" alt="${element.name}" width="200px"/>
+        </figure>`;
+    } else {
+        myFigure = `<figure>
+        <h2 class="apestas">🤢🤮</h1>
+        <h3>What a BAD Concert!</h2>
+        <img src="https://images-eu.ssl-images-amazon.com/images/I/31AriBG8mVL._SY264_BO1,204,203,200_QL40_ML2_.jpg">
+        </figure>`;
+    }
+
+    printToDocument(container, myFigure);
+};
+
+const printToDocument = (container, element) => {
+    container.innerHTML += element;
+};
+
+iterateList(classical, albumsClassical);
+iterateList(jazz, albumsJazz);
+iterateList(electronic, albumsElectronic);
+iterateList(postRock, albumsPostRock);
+
+
+
+//EXPLICACIOn a Nuria :3
+//const generateHTML = (element, container) => {
+//    let myFigure;
+//    if ("Canada" in element) {
+//       myFigure = `<figure>
+//        <h2 class="apestas">🤢🤮</h2>
+//        <img src="" >
+//        </figure>`;
+//    } else {
+//        myFigure = `<figure>
+//        <h2>${element.name} - ${element.year}</h2>
+//        <h3>${element.artist}</h3>
+//        <img src="${element.image}" alt="${element.name}" width="200px"/>
+//        </figure>`;
+//    }
+//}
